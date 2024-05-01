d511831d61458af573687a3582d2280f5bb9de45
rafik-ben1 <gokub531@gmail.com>
2024-05-01 20:18:25 +0100

    idk

---
+++ src/main/java/com/example/projet/models/listeners/SearchListener.java
@@ -0,0 +1,11 @@
+package com.example.projet.models.listeners;
+
+import com.example.projet.models.User;
+
+import java.util.List;
+
+public interface SearchListener {
+     void listen(List<User> users);
+
+    ;
+}
